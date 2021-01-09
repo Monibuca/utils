070841a6a70994cf81614e8e7cff835e4c0a67e3
langhuihui <178529795@qq.com>
2021-01-09 17:37:15 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,24 @@
+package utils
+
+import (
+	"log"
+	"net/http"
+
+	"golang.org/x/sync/errgroup"
+)
+
+// ListenAddrs Listen http and https
+func ListenAddrs(addr, addTLS, cert, key string, handler http.HandlerFunc) {
+	var g errgroup.Group
+	if addTLS != "" {
+		g.Go(func() error {
+			return http.ListenAndServeTLS(addTLS, cert, key, handler)
+		})
+	}
+	if addr != "" {
+		g.Go(func() error { return http.ListenAndServe(addr, handler) })
+	}
+	if err := g.Wait(); err != nil {
+		log.Fatal(err)
+	}
+}
