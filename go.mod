5f61388ed843afa36c7d70650dc7052d43cf461d
langhuihui <178529795@qq.com>
2021-01-25 20:33:42 +0800

    rtmp迁移新架构

--- go.mod
+++ go.mod
@@ -1,5 +1,10 @@
 module github.com/Monibuca/utils/v3
 
-go 1.15
+go 1.13
 
-require golang.org/x/sync v0.0.0-20201207232520-09787c993a3a
+require (
+	github.com/funny/slab v0.0.0-20180511031532-b1fad5e5d478
+	github.com/logrusorgru/aurora v2.0.3+incompatible
+	github.com/mattn/go-colorable v0.1.8
+	golang.org/x/sync v0.0.0-20201207232520-09787c993a3a
+)
--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package utils
 
 import (
+	"bufio"
+	"io"
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
+	"runtime"
 
 	"golang.org/x/sync/errgroup"
 )
@@ -21,4 +26,43 @@ func ListenAddrs(addr, addTLS, cert, key string, handler http.HandlerFunc) {
 	if err := g.Wait(); err != nil {
 		log.Fatal(err)
 	}
+}
+
+// 检查文件或目录是否存在
+// 如果由 filename 指定的文件或目录存在则返回 true，否则返回 false
+func Exist(filename string) bool {
+	_, err := os.Stat(filename)
+	return err == nil || os.IsExist(err)
+}
+
+func ReadFileLines(filename string) (lines []string, err error) {
+	file, err := os.OpenFile(filename, os.O_RDONLY, 0644)
+	if err != nil {
+		return
+	}
+	defer file.Close()
+
+	bio := bufio.NewReader(file)
+	for {
+		var line []byte
+
+		line, _, err = bio.ReadLine()
+		if err != nil {
+			if err == io.EOF {
+				file.Close()
+				return lines, nil
+			}
+			return
+		}
+
+		lines = append(lines, string(line))
+	}
+}
+
+func CurrentDir(path ...string) string {
+	_, currentFilePath, _, _ := runtime.Caller(1)
+	if len(path) == 0 {
+		return filepath.Dir(currentFilePath)
+	}
+	return filepath.Join(filepath.Dir(currentFilePath), filepath.Join(path...))
 }
